f8dba501c61b26bfec7a07ae41091cdf4c1b5d00
Jun <wnsghd1@naver.com>
2023-03-15 02:28:49 +0900

    섭씨 > 화씨변환 프로그램 주석 추가

--- temperature.py
+++ temperature.py
@@ -1,5 +1,8 @@
-celsius = int(input("섭씨 온도를 입력하세요 : "))
+# 변수 celsius에 섭씨온도를 int형 값으로 입력받음
+celsius = int(input("섭씨 온도를 입력하세요 : ")) 
 
+# 섭씨 온도를 화씨 온도로 변환한 후 fahrenheit 변수에 입력
 fahrenheit = 9 / 5 * celsius + 32
 
+# fahrenheit변수를 출력
 print("화씨 온도는 : ", fahrenheit)
