d54e9233f3a4819c300b9d3882ed92264540e997
Jun <jun@Junui-MacBookPro.local>
2023-05-02 23:47:31 +0900

    and, or, not 연산자

--- cone.py
+++ cone.py
@@ -3,11 +3,13 @@ rad = int(input("반지름을 입력하세요 : "))
 hei = int(input("높이를 입력하세요 : "))
 
 #python은 4칸을 들여쓰기 해주면 블럭이 지정됨
-if hei > 0 :
+if rad > 0 :
     # 부피 계산 출력
-    print("부피는 : ", 1/3 * 3.14 * rad ** 2 * hei, "입니다.")
+    vol = 1/3 * 3.14 * rad ** 2 * hei
+    print("부피는 : ", vol, "입니다.")
 
     #겉넓이 계산 출력
-    print(3.14 * rad ** 2 + 3.14 * rad * hei)
+    suf = 3.14 * rad ** 2 + 3.14 * rad * hei
+    print("원뿔의 넓이는 : ",suf,"입니다.")
 else : 
     print("반지름에 양수값을 넣어주세요")
---
+++ logicalOperator.py
@@ -0,0 +1,22 @@
+temp = 20
+fruit = "apple"
+
+# AND
+# AND연산은 and
+print(temp >= 27 and fruit =="pear")
+print(temp >= 27 and fruit =="apple")
+print(temp >= 19 and fruit =="apple")
+
+# OR
+# OR연산은 or
+print(temp >= 27 or fruit =="pear")
+print(temp >= 27 or fruit =="apple")
+print(temp >= 19 or fruit =="apple")
+
+# NOT - 단항 연산자로 논리반전
+# not을 붙임
+print(temp <= 27)
+print(not temp <= 27)
+
+# shor-circuit evaluation
+# 첫 번째 논리값 만으로 전체 연산 결과가 판별 가능할 때 두 번째 논리값은 확인(평가)하지 않는 기법
