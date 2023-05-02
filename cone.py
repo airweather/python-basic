bae829fb04871c531d9a88e45f5d8d8b3055bd18
Jun <jun@Junui-MacBookPro.local>
2023-05-02 23:50:18 +0900

    원뿔 계산기에 and 연산 추가

--- cone.py
+++ cone.py
@@ -3,7 +3,7 @@ rad = int(input("반지름을 입력하세요 : "))
 hei = int(input("높이를 입력하세요 : "))
 
 #python은 4칸을 들여쓰기 해주면 블럭이 지정됨
-if rad > 0 :
+if rad > 0 and hei > 0:
     # 부피 계산 출력
     vol = 1/3 * 3.14 * rad ** 2 * hei
     print("부피는 : ", vol, "입니다.")
