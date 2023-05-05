66751efebf548ca09c7efab6e21a08cc8ae68f6e
J <j@Jui-MacBookAir.local>
2023-05-06 04:36:22 +0900

    여러 값을 리턴받아 변수에 동시할당 하는 함수

--- variable.py
+++ variable.py
@@ -5,3 +5,14 @@ temp, season = 27, "summer"
 # 교환(swap)
 # 복수 개의 변수에 할당하는 값을 맞바꿈
 temp, season = season, temp
+
+# 세 개의 값을 입력받아 오름차순으로 정렬하는 함수
+a = int(input("첫번째 숫자를 입력하세요:"))
+b = int(input("두번째 숫자를 입력하세요:"))
+c = int(input("세번째 숫자를 입력하세요:"))
+
+def sort3(a, b, c):
+    if a > b:
+        a, b = b, a
+    if a > c:
+        a, c = c, a
