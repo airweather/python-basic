10ed78969ff6945cc7ee9d3964e758ef3dcd8231
Jun <wnsghd1@naver.com>
2023-05-05 23:29:51 +0900

    구구단

--- gugudan.py
+++ gugudan.py
@@ -1,7 +1,17 @@
+print(format("구구단표", ">20s"))
 
+# 표 머리 출력
+print("  |", end="")
+for j in range(1, 10):
+    print("  ", j, end="")
 
-number = [1,2,3,4,5,6,7,8,9]
+# 새로운 행 삽입
+print()
+print("----------------------------------------")
 
-for num in number:
-    for gugu in number:
-        print(gugu*num)
+# 구구단 표 출력
+for i in range(1, 10, 1):
+    print(i, "|", end="")
+    for j in range(1, 10, 1) :
+        print(format(i*j, "4d"), end="")
+    print()
