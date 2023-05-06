e1d8479c6bfaa43df34fd6f3c4bb44b43f124389
Jun <wnsghd1@naver.com>
2023-05-07 04:31:46 +0900

    각종 str 메소드

---
+++ strMethod.py
@@ -0,0 +1,18 @@
+# upper(), lower()
+# 대, 소문자로 변경
+
+# title()
+# 각단어의 첫 글자를 대문자로 변경
+
+# strip(), rstrip(), lstrip()
+# 양쪽, 왼쪽, 오른쪽의 특정 문자를 제거
+
+# replace()
+# 문자열 특정 부분을 대체
+# "I love python".replce("o", "i")
+
+# split()
+# 구분자로 분할하여 리스트로 반환
+
+# dir(str)
+# 사용 가능한 str 메소드 확인
